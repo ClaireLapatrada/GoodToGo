5695764708046c84ce1909b71969f7d8427a4fce
Thitiwut Pattanasuttinont <93137058+aFluffyHotdog@users.noreply.github.com>
2025-03-05 18:01:51 -0500

    finished yolo function

--- mac-yolo/yoloObj.py
+++ mac-yolo/yoloObj.py
@@ -1,6 +1,8 @@
 import argparse
+import cv2
 import cv2.dnn
 import numpy as np
+import os
 
 from ultralytics.utils import ASSETS, yaml_load
 from ultralytics.utils.checks import check_yaml
@@ -36,90 +38,90 @@ def add_model(model_name: str, model_path: str, class_path: str):
     model_dict[model_name] = cv2.dnn.readNetFromONNX(model_path)
     class_dict[model_name] = yaml_load(check_yaml(class_path))["names"]
 
-def main(models, input_image):
+def detect_defects(models, input_images, file_path):
     """
     Main function to load ONNX models, perform inference, draw bounding boxes, and display the output image.
 
     Args:
         models (list): List of models initialized as OpenCV dnn objects.
-        input_image (str): Path to the input image.
+        input_images list[str]: list of paths to the image
+        file_path (str): the designated folder for the image to be saved.
     """
-    # Read the input image
-    original_image = cv2.imread(input_image)
-    [height, width, _] = original_image.shape
+    for image in input_images:
+        # Read the input image
+        original_image = cv2.imread(image)
+        [height, width, _] = original_image.shape
 
-    # Prepare a square image for inference
-    length = max((height, width))
-    image = np.zeros((length, length, 3), np.uint8)
-    image[0:height, 0:width] = original_image
+        # Prepare a square image for inference
+        length = max((height, width))
+        image = np.zeros((length, length, 3), np.uint8)
+        image[0:height, 0:width] = original_image
 
-    # Calculate scale factor
-    scale = length / 640
+        # Calculate scale factor
+        scale = length / 640
 
-    # Perform inference for all models
-    detections = []
-    for model_name, model in models.items():
-        blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
-        model.setInput(blob)
+        # Perform inference for all models
+        detections = []
+        for model_name, model in models.items():
+            blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255, size=(640, 640), swapRB=True)
+            model.setInput(blob)
 
-        # Perform inference
-        outputs = model.forward()
+            # Perform inference
+            outputs = model.forward()
 
-        # Prepare output array
-        outputs = np.array([cv2.transpose(outputs[0])])
-        rows = outputs.shape[1]
+            # Prepare output array
+            outputs = np.array([cv2.transpose(outputs[0])])
+            rows = outputs.shape[1]
 
-        boxes = []
-        scores = []
-        class_ids = []
+            boxes = []
+            scores = []
+            class_ids = []
 
-        # Iterate through output to collect bounding boxes, confidence scores, and class IDs
-        for i in range(rows):
-            classes_scores = outputs[0][i][4:]
-            (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
-            if maxScore >= 0.25:
-                box = [
-                    outputs[0][i][0] - (0.5 * outputs[0][i][2]),
-                    outputs[0][i][1] - (0.5 * outputs[0][i][3]),
-                    outputs[0][i][2],
-                    outputs[0][i][3],
-                ]
-                boxes.append(box)
-                scores.append(maxScore)
-                class_ids.append(maxClassIndex)
+            # Iterate through output to collect bounding boxes, confidence scores, and class IDs
+            for i in range(rows):
+                classes_scores = outputs[0][i][4:]
+                (minScore, maxScore, minClassLoc, (x, maxClassIndex)) = cv2.minMaxLoc(classes_scores)
+                if maxScore >= 0.25:
+                    box = [
+                        outputs[0][i][0] - (0.5 * outputs[0][i][2]),
+                        outputs[0][i][1] - (0.5 * outputs[0][i][3]),
+                        outputs[0][i][2],
+                        outputs[0][i][3],
+                    ]
+                    boxes.append(box)
+                    scores.append(maxScore)
+                    class_ids.append(maxClassIndex)
 
-        # Apply NMS (Non-maximum suppression)
-        result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
+            # Apply NMS (Non-maximum suppression)
+            result_boxes = cv2.dnn.NMSBoxes(boxes, scores, 0.25, 0.45, 0.5)
 
-        # Iterate through NMS results to draw bounding boxes and labels
-        for i in range(len(result_boxes)):
-            index = result_boxes[i]
-            box = boxes[index]
-            detection = {
-                "model": model_name,
-                "class_id": class_ids[index],
-                "class_name": class_dict[model_name][class_ids[index]],
-                "confidence": scores[index],
-                "box": box,
-                "scale": scale,
-            }
-            detections.append(detection)
-            if detection["confidence"] >=  0.2:
-                draw_bounding_box(
-                    original_image,
-                    class_ids[index],
-                    scores[index],
-                    round(box[0] * scale),
-                    round(box[1] * scale),
-                    round((box[0] + box[2]) * scale),
-                    round((box[1] + box[3]) * scale),
-                    class_dict[model_name]
-                )
+            # Iterate through NMS results to draw bounding boxes and labels
+            for i in range(len(result_boxes)):
+                index = result_boxes[i]
+                box = boxes[index]
+                detection = {
+                    "model": model_name,
+                    "class_id": class_ids[index],
+                    "class_name": class_dict[model_name][class_ids[index]],
+                    "confidence": scores[index],
+                    "box": box,
+                    "scale": scale,
+                }
+                detections.append(detection)
+                if detection["confidence"] >=  0.3:
+                    draw_bounding_box(
+                        original_image,
+                        class_ids[index],
+                        scores[index],
+                        round(box[0] * scale),
+                        round(box[1] * scale),
+                        round((box[0] + box[2]) * scale),
+                        round((box[1] + box[3]) * scale),
+                        class_dict[model_name]
+                    )
 
-    # Display the image with bounding boxes
-    cv2.imshow("image", original_image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+        # Display the image with bounding boxes
+        cv2.imwrite(os.path.join(file_path , image), original_image)
 
 if __name__ == "__main__":
     # Define models and their corresponding class YAMLs
@@ -139,4 +141,4 @@ if __name__ == "__main__":
         add_model(model_name, paths["weights"], paths["classes"])
 
     colors = np.random.uniform(0, 255, size=(len(class_dict["model1"]), 3))  # Adjust color size to fit all class names
-    main(model_dict, "samples/hole.jpg")
+    detect_defects(model_dict, ["samples/hole.jpg"], "mac-yolo/samples" )
